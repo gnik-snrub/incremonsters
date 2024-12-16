9c6656588bb566973b6ae245d4910f553c1e5b65
Josiah Morris <josiahbmorris@hotmail.com>
2024-12-17 05:53:05 +0900

    Added ids to Monster type

--- src/lib/types/monster.ts
+++ src/lib/types/monster.ts
@@ -1,4 +1,5 @@
 export type Monster = {
+  id: String
   name: String,
   hp: number,
   current_hp: number,
@@ -10,14 +11,15 @@ export type Monster = {
 }
 
 export function isMonster(obj: any): obj is Monster {
-  return typeof obj === "object" &&
-    typeof obj.name === "string" &&
-    typeof obj.hp === "number" &&
-    typeof obj.current_hp === "number" &&
-    typeof obj.atk === "number" &&
-    typeof obj.def === "number" &&
-    typeof obj.spd === "number" &&
-    typeof obj.exp === "number" &&
-    typeof obj.lvl === "number"
+  return typeof obj === 'object' &&
+    typeof obj.id === 'string' &&
+    typeof obj.name === 'string' &&
+    typeof obj.hp === 'number' &&
+    typeof obj.current_hp === 'number' &&
+    typeof obj.atk === 'number' &&
+    typeof obj.def === 'number' &&
+    typeof obj.spd === 'number' &&
+    typeof obj.exp === 'number' &&
+    typeof obj.lvl === 'number'
 }
 
