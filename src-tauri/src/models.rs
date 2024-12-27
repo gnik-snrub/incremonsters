f3962399b7a6ec43954471c34d134581ec772859
Josiah Morris <josiahbmorris@hotmail.com>
2024-12-28 06:36:57 +0900

    Added derive PartialEq and PartialOrd to Monster struct

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -2,7 +2,7 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, PartialOrd)]
 pub struct Monster {
     pub id: String,
     pub name: String,
