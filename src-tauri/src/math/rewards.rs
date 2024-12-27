b7cd0535720fbee7071e121e9e44d530f88e9c79
Josiah Morris <josiahbmorris@hotmail.com>
2024-12-28 06:37:39 +0900

    Added leveling up to Rust reward calculation

--- src-tauri/src/math/rewards.rs
+++ src-tauri/src/math/rewards.rs
@@ -1,4 +1,4 @@
-use crate::models::Monster;
+use crate::{models::Monster, monsters::level_up};
 use rand::Rng;
 
 const REWARD_BASE: f32 = 10.0;
@@ -11,7 +11,7 @@ fn get_rewards(dungeon_lvl: i32, slain: &Vec<Monster>) -> i32 {
         let variance: f32 = rng.gen_range(0.8..1.2);
         reward_total += (REWARD_BASE * REWARD_GROWTH.powf(monster.lvl as f32)) * variance * dungeon_lvl as f32;
     }
-    reward_total.ceil() as i32
+    ((reward_total * 100.0).round() / 100.0) as i32
 }
 
 fn get_exp(player_team_size: i32, average_team_level: i32, slain: Monster) -> i32 {
@@ -22,13 +22,31 @@ fn get_exp(player_team_size: i32, average_team_level: i32, slain: Monster) -> i3
     exp as i32
 }
 
+fn calc_level_up(mut player: Vec<Monster>, exp: i32) -> Vec<Monster> {
+    for mut monster in player.iter_mut() {
+        let mut monsters_exp: i32 = exp + monster.exp;
+        monster.exp = 0;
+        while monsters_exp >= monster.lvl.pow(3) {
+            monsters_exp -= monster.lvl.pow(3);
+            level_up(&mut monster);
+        }
+        monster.exp = std::cmp::max(monsters_exp, 0);
+    }
+    player
+}
+
 #[tauri::command]
 pub fn win_battle_rewards(dungeon_lvl: i32, mut player: Vec<Monster>, enemy: Vec<Monster>) -> (Vec<Monster>, i32) {
     let rewards: i32 = get_rewards(dungeon_lvl, &enemy);
     let mut exp_total: i32 = 0;
-    let average_team_level: i32 = player.iter().map(|m| m.lvl).sum::<i32>() / player.len() as i32;
+    let average_team_level: i32 = if player.is_empty() {
+        1
+    } else {
+        player.iter().map(|m| m.lvl).sum::<i32>() / player.len() as i32
+    };
     for monster in enemy.iter() {
         exp_total += get_exp(player.len() as i32, average_team_level, monster.clone());
     }
+    player = calc_level_up(player, exp_total);
     (player, rewards)
 }
