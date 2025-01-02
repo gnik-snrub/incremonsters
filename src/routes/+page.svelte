d4f7bc9da50889cf10d71342cde03ab9f9ab3aa8
Josiah Morris <josiahbmorris@hotmail.com>
2025-01-03 07:06:05 +0900

    Added team changing from the monster management section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -103,6 +103,36 @@
       })
   }
 
+function handleDragStart(event: DragEvent) {
+  event.dataTransfer?.setData('text/plain', event.target?.dataset.idx)
+}
+
+function handleDragOver(event: DragEvent) {
+  event.preventDefault();
+}
+
+function handleDrop(event: DragEvent) {
+  event.preventDefault();
+  const stableIdx = event.dataTransfer?.getData('text/plain') || -1
+  const squadIdx = event.currentTarget?.dataset.idx || -1
+
+  if (stableIdx === -1 || squadIdx === -1) {
+    return
+  }
+
+  const tempMonster = playerSquad.getMonsters()[squadIdx]
+
+  playerSquad.setMonsters([
+    ...playerSquad.getMonsters().slice(0, squadIdx),
+    stable.getMonsters()[stableIdx],
+    ...playerSquad.getMonsters().slice(Number(squadIdx) + 1)
+  ])
+  stable.setMonsters([
+    ...stable.getMonsters().slice(0, stableIdx),
+    ...stable.getMonsters().slice(Number(stableIdx) + 1),
+    tempMonster
+  ].sort((a, b) => a.lvl - b.lvl))
+}
 </script>
 
 <main class="h-screen p-4 bg-black grid gap-4 grid-cols-6 grid-rows-4">
@@ -111,12 +141,27 @@
   <section class="bg-yellow-500 col-start-3 row-start-2">Prestige</section>
   <section class={`col-start-4 row-start-2 ${pendingDungeonLevelIncrease ? 'bg-purple-200' : 'bg-purple-500'}`} onclick={nextDungeon}>
       {pendingDungeonLevelIncrease ? "Next dungeon!" : "Automation"}</section>
-  <section class="h-full p-4 overflow-y-auto bg-red-500 col-span-2 row-span-2 col-start-5 row-start-1 grid grid-cols-4 gap-4 auto-rows-[25%] place-items-center">
-    {#each stable.getMonsters() as monster}
-      <div class="w-full h-full border-4 rounded-full border-rose-800 bg-rose-400 grid place-items-center max-w-24 max-h-24">
-        <h3>{monster.name}</h3>
+  <section class="h-full p-4 bg-red-500 col-span-2 row-span-2 col-start-5 row-start-1 grid grid-rows-[auto_min-content] grid-cols-1">
+    <section class="w-full h-full overflow-y-auto grid grid-rows-[1em_auto] gap-4 place-items-center">
+      <h4>Stable</h4>
+      <div class="h-full w-full overflow-y-auto grid grid-cols-4 gap-4 auto-rows-[25%] place-items-center">
+        {#each stable.getMonsters() as monster, idx}
+          <div ondragstart={handleDragStart} data-idx={idx} draggable="true" class="w-full h-full border-4 rounded-full border-rose-800 bg-rose-400 grid place-items-center max-w-24 max-h-24">
+            <h3 class="select-none">{monster.name}</h3>
+          </div>
+        {/each}
       </div>
-    {/each}
+    </section>
+    <section class="w-full p-4 border-black border-t-2 grid grid-rows-[1em_auto] gap-4 place-items-center">
+      <h4 class="h-min">Squad</h4>
+      <div class="w-full grid grid-cols-4 place-items-center">
+      {#each playerSquad.getMonsters() as monster, idx}
+        <div ondrop={handleDrop} ondragover={handleDragOver} data-idx={idx} class="w-full h-full border-4 rounded-full hover:border-amber-400 border-amber-800 bg-amber-400 grid place-items-center max-w-24 max-h-24">
+          <h3 class="select-none">{monster.name}</h3>
+        </div>
+      {/each}
+      </div>
+    </section>
   </section>
   <section class="flex flex-col w-full bg-green-500 align-center row-span-2 col-span-6 row-start-3">
     <div class="flex flex-row justify-center w-full gap-10">
