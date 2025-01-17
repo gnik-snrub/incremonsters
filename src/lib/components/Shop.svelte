dc8903f969a53b26a102cd2e421dce2c82d8545c
Josiah Morris <josiahbmorris@hotmail.com>
2025-01-18 02:11:48 +0900

    Added display of descriptions on hover to Shop items

--- src/lib/components/Shop.svelte
+++ src/lib/components/Shop.svelte
@@ -12,13 +12,26 @@
     }
   }
 
+  let itemDescription = $state("")
+
+  function handleEnter(event: Event) {
+    itemDescription = (event.currentTarget as HTMLButtonElement).dataset.description || ""
+  }
+
+  function handleExit() {
+    itemDescription = ""
+  }
 </script>
 
-<section class="bg-orange-500 col-span-2 row-span-2 grid grid-cols-4 overflow-y-auto grid-rows-[25%] gap-4 p-4">
-  {#each items as item}
-    <button class="p-1 bg-orange-300 border-2 border-black max-h-28" onclick={() => {buyItem(item)}}>
-      <p>{item.name} ({item.amountBought()})</p>
-      <p>Cost: {item.nextCost()}</p>
-    </button>
-  {/each}
+<section class="flex flex-col items-center p-4 bg-orange-500 col-span-2 row-span-2">
+  <ul class="h-5/6 grid grid-cols-4 overflow-y-auto auto-rows-[25%] w-full gap-4">
+    {#each items as item}
+      <button class="p-1 bg-orange-300 border-2 border-black max-h-28" data-description={item.description}
+        onmouseenter={handleEnter} onmouseleave={handleExit} onclick={() => {buyItem(item)}}>
+        <p>{item.name} ({item.amountBought()})</p>
+        <p>Cost: {item.nextCost()}</p>
+      </button>
+    {/each}
+  </ul>
+  <p class="w-5/6 pt-2 mt-2 text-center border-t-2 border-black">{itemDescription}</p>
 </section>
