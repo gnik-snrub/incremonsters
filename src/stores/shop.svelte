62e47c982605f5bedf33ff31816cf8de4c18df92
Josiah Morris <josiahbmorris@hotmail.com>
2025-01-26 04:19:38 +0900

    Renamed shop items to sound less generic

--- src/stores/shop.svelte
+++ src/stores/shop.svelte
@@ -4,15 +4,15 @@
   import { type IntermissionEffect, type BoostEffect } from "$lib/types/shop"
   import { intermissionEffect, boostEffect } from "$entities/shop"
 
-  export const atkBoost: BoostEffect = $state(boostEffect("Atk Boost", 10, 1.25, "Raises player monster attack by 10% (additive)", "atk", 0.1))
-  export const defBoost: BoostEffect = $state(boostEffect("Def Boost", 10, 1.25, "Raises player monster defense by 10% (additive)", "def", 0.1))
-  export const spdBoost: BoostEffect = $state(boostEffect("Spd Boost", 10, 1.25, "Raises player monster speed by 10% (additive)", "spd", 0.1))
-  export const hpBoost: BoostEffect = $state(boostEffect("HP Boost", 10, 1.25, "Raises player monster HP by 10% (additive)", "hp", 0.1))
+  export const atkBoost: BoostEffect = $state(boostEffect("Spinach", 10, 1.25, "Raises player monster attack by 10% (additive)", "atk", 0.1))
+  export const defBoost: BoostEffect = $state(boostEffect("Iron Supplement", 10, 1.25, "Raises player monster defense by 10% (additive)", "def", 0.1))
+  export const spdBoost: BoostEffect = $state(boostEffect("Coffee", 10, 1.25, "Raises player monster speed by 10% (additive)", "spd", 0.1))
+  export const hpBoost: BoostEffect = $state(boostEffect("Chicken Dinner", 10, 1.25, "Raises player monster HP by 10% (additive)", "hp", 0.1))
 
   export const monsterBoosts: BoostEffect[] = [atkBoost, defBoost, spdBoost, hpBoost]
 
-  export const expBoost: BoostEffect = $state(boostEffect("EXP Boost", 10, 1.25, "Raises player monster EXP by 10% (additive)", "exp", 0.1))
-  export const goldBoost: BoostEffect = $state(boostEffect("Gold Boost", 10, 1.25, "Gold gain increase by 10% (additive)", "gold", 0.1))
+  export const expBoost: BoostEffect = $state(boostEffect("Strategy Guide", 10, 1.25, "Raises player monster EXP by 10% (additive)", "exp", 0.1))
+  export const goldBoost: BoostEffect = $state(boostEffect("Piggy Bank", 10, 1.25, "Gold gain increase by 10% (additive)", "gold", 0.1))
 
   export const rewardBoosts: BoostEffect[] = [expBoost, goldBoost]
 
