94485038bfd788cff37c8a150c1ba65533973c2e
Josiah Morris <josiahbmorris@hotmail.com>
2025-02-21 03:08:28 +0900

    Broke store items into separate pages

--- src/lib/components/Shop.svelte
+++ src/lib/components/Shop.svelte
@@ -3,7 +3,9 @@
   import type { StoreItem } from "$lib/types/storeitem"
   import { monsterBoosts, rewardBoosts, intermissionEffects } from "../../stores/shop.svelte"
 
-  const items = [...monsterBoosts, ...rewardBoosts, ...intermissionEffects]
+  const boosts = [...monsterBoosts, ...rewardBoosts]
+  const campUpgrades = [...intermissionEffects]
+  const equipment: StoreItem[] = []
 
   function buyItem(item: StoreItem) {
     if (gold.get() >= item.nextCost()) {
@@ -21,17 +23,25 @@
   function handleExit() {
     itemDescription = ""
   }
+
+  let shopItems: StoreItem[] = $state(boosts)
+
 </script>
 
 <section class="flex flex-col items-center p-4 bg-orange-500 col-span-2 row-span-2">
-  <ul class="h-5/6 grid grid-cols-4 overflow-y-auto auto-rows-[25%] w-full gap-4">
-    {#each items as item}
-      <button class="p-1 bg-orange-300 border-2 border-black max-h-28" data-description={item.description}
-        onmouseenter={handleEnter} onmouseleave={handleExit} onclick={() => {buyItem(item)}}>
-        <p>{item.name} ({item.amountBought()})</p>
-        <p>Cost: {item.nextCost()}</p>
-      </button>
-    {/each}
+  <ul class="flex flex-row w-full align-start [&>*]:border-r-2 [&>*]:border-black [&>*]:w-1/4 text-center pb-4">
+    <li onclick={() => shopItems = boosts}>Boosts</li>
+    <li onclick={() => shopItems = campUpgrades}>Camp Upgrades</li>
+    <li onclick={() => shopItems = equipment}>Equipment</li>
   </ul>
-  <p class="w-5/6 pt-2 mt-2 text-center border-t-2 border-black">{itemDescription}</p>
+    <ul class="h-5/6 grid grid-cols-4 overflow-y-auto auto-rows-[25%] w-full gap-4">
+      {#each shopItems as item}
+        <button class="p-1 bg-orange-300 border-2 border-black max-h-28" data-description={item.description}
+          onmouseenter={handleEnter} onmouseleave={handleExit} onclick={() => {buyItem(item)}}>
+          <p>{item.name} ({item.amountBought()})</p>
+          <p>Cost: {item.nextCost()}</p>
+        </button>
+      {/each}
+    </ul>
+  <p class="h-1.5 w-5/6 pt-2 mt-auto mb-6 text-center border-t-2 border-black">{itemDescription}</p>
 </section>
