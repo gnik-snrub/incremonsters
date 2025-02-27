06ae6502ef97ada445f764d89861e8c487591d65
Josiah Morris <josiahbmorris@hotmail.com>
2025-02-28 03:45:10 +0900

    Removed unneeded Trait struct definition

--- src-tauri/src/traits/mod.rs
+++ src-tauri/src/traits/mod.rs
@@ -2,15 +2,6 @@ use crate::models::Trait;
 
 pub mod stonekin;
 
-pub struct Trait {
-    name: String,
-    description: String,
-    trigger: Trigger,
-    #[serde(skip)]
-    #[serde(default = "default_callback")]
-    callback: CallbackFn,
-}
-
 pub trait MonsterTrait {
     fn get(&self) -> Trait;
 }
