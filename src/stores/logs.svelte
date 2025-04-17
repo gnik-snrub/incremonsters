8105921bd856b4dd8c79650c5c0837469953494c
Josiah Morris <josiahbmorris@hotmail.com>
2025-04-18 03:08:45 +0900

    Added logs store to Svelte end

---
+++ src/stores/logs.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" module>
+  export const logStore = logStoreFactory()
+
+  function logStoreFactory() {
+    let logs: String[] = $state([])
+
+    function addToStore(updates: String[]) {
+      logs = [...logs, ...updates]
+    }
+
+    function readLogs() {
+      return logs
+    }
+
+    return { addToStore, readLogs }
+  }
+</script>
